28c096778d1f81c6c26204de08ea9cf04f313aac
Diogo Bernini <diogo@diogobernini.com>
2016-11-08 13:11:22 -0200

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,8 @@
-let Visualizador = new DViewer("#image", "#btnPlus", "#btnLess", "#btn100", "img2.jpg");
+var Visualizador = new DViewer("#image", "#btnPlus", "#btnLess", "#btn100", "img2.jpg");
 
 Visualizador.setHeight(50);
 
-let img1 = false;
+var img1 = false;
 
 $("#changeImg").click(function(event) {
     if(img1){
